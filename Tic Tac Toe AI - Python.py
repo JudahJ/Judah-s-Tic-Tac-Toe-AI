b5b811d66a7d936b476167dce8529ad40eaa8042
JudahJ <121975642+JudahJ@users.noreply.github.com>
2024-12-04 09:47:55 -0800

    Update Tic Tac Toe AI - Python.py

--- Tic Tac Toe AI - Python.py
+++ Tic Tac Toe AI - Python.py
@@ -333,7 +333,7 @@ class SmartRandomAI:
 
 if __name__ == "__main__":
     player1 = HumanPlayer('O')
-    player2 = AIPlayer('X', JudahsMiniMax())
-    #player2 = AIPlayer('X', NoahJudahMiniMax(9))
+    #player2 = AIPlayer('X', JudahsMiniMax())
+    player2 = AIPlayer('X', NoahJudahMiniMax(9))
     game = TicTacToe(player1, player2)
     game.play()
